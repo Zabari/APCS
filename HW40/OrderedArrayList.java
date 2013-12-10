3245a41987e4d6207aa0bea78dcf72112d10e7f6
Zabari <Zabari.daniel@Gmail.com>
2013-12-09 23:00:04 -0500

    HW40

--- HW40/OrderedArrayList.java
+++ HW40/OrderedArrayList.java
@@ -1,3 +1,7 @@
+// DANIEL ZABARI
+// pd 9
+// HW40
+// 2013-12-10
 /*============================================
    class OrderedArrayList
    Wrapper class for ArrayList.
@@ -55,7 +59,14 @@ public class OrderedArrayList {
     // maintains ascending order of elements
     // uses a linear search to find appropriate index
     public void addLinear(Comparable newVal) { 
-	// *** YOUR IMPLEMENTATION HERE ***
+	int i=0;
+
+	    while (i<_data.size()&& (_data.get(i).compareTo(newVal)<=0)){
+		i++;
+	    }
+	    _data.add(i,newVal);
+
+	
     }
 
 
@@ -65,31 +76,58 @@ public class OrderedArrayList {
     // maintains ascending order of elements
     // uses a binary search to find appropriate index
     public void addBinary(Comparable newVal) { 
-	int tPos = -1;
-        int m = (lo + hi) / 2; 
+	int hi=0;
+	int lo=0;
+	if (_data.size()>0){
+	    if (_data.get(_data.size()-1).compareTo(newVal)<0){
+		_data.add(newVal);
+	    }
+	    else if (_data.get((_data.size()-1)/2).compareTo(newVal)>0){
+		hi=_data.size()-1/2;
+		lo=0;;
+	    }
+	    else{
+		hi=_data.size()-1;
+		lo=(_data.size()-1)/2;
+	    }
+	    int m = (lo + hi) / 2; 
 
-        while( lo <= hi ) { 
-	    m = (lo + hi) / 2;
-
+	    while( lo <= hi ) { 
+		m = (lo + hi) / 2;
  
-            if ( a[m].compareTo(target) == 0 ) 
-                return m;
+		if ( _data.get(m).compareTo(newVal) >0)
+		    hi=m-1;
 
-
-            else if ( a[m].compareTo(target) > 0 ) 
-                hi = m - 1;
-            else if ( a[m].compareTo(target) < 0 ) 
-                lo = m + 1;
-        }
-	_data.add(tPos,newVal);
-
+		else if (_data.get(m).compareTo(newVal)<0)
+		    lo = m + 1;
+		else if (_data.get(m).compareTo(newVal)==0){
+		    _data.add(m,newVal);
+		    break;
+		}
+		if (hi-lo<=1){
+		    if (_data.get(lo).compareTo(newVal)>0){
+			_data.add(lo, newVal);
+			break;
+		    }
+		    else if (_data.get(hi).compareTo(newVal)>0){
+			_data.add(hi, newVal);
+			break;
+		    }
+		    else if (_data.get(hi).compareTo(newVal)<0){
+			_data.add(hi+1, newVal);
+			break;
+		    }
+		}
+	    }
+	}
+	else {_data.add(0,newVal);}
     }	
 
 
     // main method solely for testing purposes
     public static void main( String[] args ) {
 
-	/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 	OrderedArrayList Franz = new OrderedArrayList();
 
 	// testing linear search
@@ -102,6 +140,7 @@ public class OrderedArrayList {
 	for( int i = 0; i < 15; i++ ) 
 	    Franz.addBinary( (int)( 50 * Math.random() ) );
 	System.out.println( Franz );
+	/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
     }
 
